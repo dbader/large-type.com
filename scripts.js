e3d48eff5efe552c0218ca87a5092ee67b3f8d4d
Daniel Bader <mail@dbader.org>
2015-12-18 22:58:00 +0100

    Update scripts.js
    - Use textContent instead of innerHTML
    - URL decode the fragment (so we can support spaces on Safari and FF)
    - Wrap everything in a self-executing func

--- scripts.js
+++ scripts.js
@@ -9,29 +9,31 @@
 // URL fragment won't be sent as part of the request. Thus a-okay to share
 // https://stackoverflow.com/questions/317760/how-to-get-url-hash-from-server-side
 
-var word = document.querySelector('.word')
-var tmpl = document.querySelector('#charbox-template');
+(function(){
+    var word = document.querySelector('.word')
+    var tmpl = document.querySelector('#charbox-template');
 
-if (!location.hash) {
-    location.hash = '*hello*';
-}
-
-function clearWord() {
-    while (word.firstChild) {
-        word.removeChild(word.firstChild);
+    if (!location.hash) {
+        location.hash = '*hello*';
     }
-}
 
-function onHashChange() {
-    clearWord();
-    var text = location.hash.slice(1, location.hash.length);
-    text.split('').forEach(function(chr) {
-        var charbox = tmpl.content.cloneNode(true);
-        charbox.querySelector('.char').innerText = chr;
-        word.appendChild(charbox);
-    });
-}
+    function clearWord() {
+        while (word.firstChild) {
+            word.removeChild(word.firstChild);
+        }
+    }
 
-window.addEventListener('hashchange', onHashChange, false);
+    function onHashChange() {
+        clearWord();
+        var text = decodeURIComponent(location.href.split('#')[1] || '');
+        text.split('').forEach(function(chr) {
+            var charbox = tmpl.content.cloneNode(true);
+            charbox.querySelector('.char').textContent = chr;
+            word.appendChild(charbox);
+        });
+    }
 
-onHashChange();
+    window.addEventListener('hashchange', onHashChange, false);
+
+    onHashChange();
+})();
